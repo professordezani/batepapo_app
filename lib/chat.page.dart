4adb4f44af7a7d424b9da1424bdbc27a13bc44bc
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-09-26 17:34:30 -0300

    Tela do Chat

--- lib/chat.page.dart
+++ lib/chat.page.dart
@@ -10,13 +10,141 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Chat"),
+        actions: [
+          IconButton(
+            onPressed: () => deslogar(context),
+            icon: Icon(Icons.logout)
+          )
+        ]
+      ),
       body: Column(
         children: [
-          Text("Chat"),
-          TextButton(
-            onPressed: () => deslogar(context),
-            child: Text("Deslogar")
-          )
+          Expanded(
+            child: ListView(
+              reverse: true,
+              children: [
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),                  
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(),
+                  title: Text("Nome do usuário"),
+                  subtitle: Text("Mensagem enviada pelo usuário"),
+                  trailing: Text("2 min."),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(5, 10, 0, 8),
+            // padding: EdgeInsets.only(left: 5, bottom: 8),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder()
+                    ),
+                  )
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.send),
+                ),
+              ],
+            )
+          )        
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
         "/login": (context) => LoginPage(),
         "/registro": (context) => RegistroPage(),
